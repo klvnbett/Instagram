846e846346c6c2b2274287461de163ae199e3359
BettK <kelvin.bett@student.moringaschool.com>
2021-09-12 13:09:09 +0300

    added image class to models

--- instaclone/models.py
+++ instaclone/models.py
@@ -1,3 +1,13 @@
 from django.db import models
-
+import datetime as dt
+from django.contrib.auth.models import User
 # Create your models here.
+
+
+class Image(models.Model):
+    image_name =models.CharField(max_length=60,blank=True)
+    image_caption=models.CharField(max_length=60,blank=True)
+    posted_On = models.DateTimeField(auto_now_add=True)
+    profile =models.ForeignKey(User)
+    user_profile=models.ForeignKey(Profile)
+    image=models.ImageField(upload_to='images/',default="Image")
