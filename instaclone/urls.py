ae8f3ae379969437597fbe280944e4a7eabcbc85
BettK <kelvin.bett@student.moringaschool.com>
2021-09-12 09:53:08 +0300

    added urls to instaclone

--- instaclone/urls.py
+++ instaclone/urls.py
@@ -1,2 +1,10 @@
 from django.conf.urls import url
 from . import views
+from  django.conf.urls import url
+from django .conf import settings
+
+
+
+urlpatterns =[
+    url(r'^$',views.index,name=index),
+]
