889962abd9c09f379df07f69f1e33595a13357cb
BettK <kelvin.bett@student.moringaschool.com>
2021-09-12 12:40:05 +0300

    updated base html

--- instagram/urls.py
+++ instagram/urls.py
@@ -21,5 +21,5 @@ urlpatterns = [
     url('admin/', admin.site.urls),
     url(r'^$',include('instaclone.urls')),
     url(r'^accounts/', include('registration.backends.simple.urls')),
-    url(r'^logout/$', auth_views.logout,{"next_page":'/'}),
+    # url(r'^logout/$', auth_views.logout,{"next_page":'/'}),
 ]
