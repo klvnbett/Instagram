ea744ac84fc8c20b7a4cf6437d93c4a42feba2d5
BettK <kelvin.bett@student.moringaschool.com>
2021-09-12 14:25:39 +0300

    made initial migrations

--- instagram/urls.py
+++ instagram/urls.py
@@ -19,7 +19,7 @@ from django.contrib.auth import views as auth_views
 
 urlpatterns = [
     url('admin/', admin.site.urls),
-    url(r'^$',include('instaclone.urls')),
+    url(r'',include('instaclone.urls')),
     url(r'^accounts/', include('registration.backends.simple.urls')),
     # url(r'^logout/$', auth_views.logout,{"next_page":'/'}),
 ]
