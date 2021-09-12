28faf6e6ea8739dda8ae5bf9b56d1e1055ce3f77
BettK <kelvin.bett@student.moringaschool.com>
2021-09-12 13:44:40 +0300

    added comment class to models

--- instaclone/models.py
+++ instaclone/models.py
@@ -8,19 +8,26 @@ class Image(models.Model):
     image_name =models.CharField(max_length=60,blank=True)
     image_caption=models.CharField(max_length=60,blank=True)
     posted_On = models.DateTimeField(auto_now_add=True)
-    profile =models.ForeignKey(User)
-    user_profile=models.ForeignKey(Profile)
+    profile =models.ForeignKey(User,on_delete=models.CASCADE,blank=True)
+    # user_profile=models.ForeignKey(Profile)
     image=models.ImageField(upload_to='images/',default="Image")
 
 
     def __str__(self):
         return self.image_name
 
-class Profile:(models.Model):
+class Profile(models.Model):
     profile_pic=models.ImageField('photo')
     Bio=models.CharField(max_length=35)
     user= models.OneToOneField(User, on_delete=models.CASCADE,null=True)
 
     def __str__(self):
         return self.user
+
+
+class Comment(models.Model):
+    Comment=models.TextField()
+    image=models.ForeignKey(Image,on_delete=models.CASCADE)
+    posted_on =models.DateTimeField(auto_now_add=True,null=True)
+
 
