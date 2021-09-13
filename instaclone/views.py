ed77086731338f4a436bcb50b2d2726985249160
BettK <kelvin.bett@student.moringaschool.com>
2021-09-13 07:46:26 +0300

    added models to admin py

--- instaclone/views.py
+++ instaclone/views.py
@@ -1,5 +1,10 @@
 from django.shortcuts import render,redirect
 from django .http import Http404,HttpResponse,HttpResponseRedirect
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth.models import User
+from .models import *
+# from .forms import *
+from django.db.models import F
 
 # Create your views here.
 def index(request):
