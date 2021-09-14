bfb863c83539da45c1c4efcd37438f32e3d4163f
BettK <kelvin.bett@student.moringaschool.com>
2021-09-14 10:56:02 +0300

    added erro htnl page

--- instaclone/urls.py
+++ instaclone/urls.py
@@ -8,6 +8,11 @@ from django.conf.urls.static import static
 
 urlpatterns =[
     url(r'^$',views.index,name='index'),
+    url(r'^accounts/profile/$',views.profile,name = 'profile'),
+    url(r'^profile/(\d+)',views.other_profile,name = 'visitprofile'),
+    url(r'^search/profile$', views.search, name='profileresults'),
+    url(r'^timeline$', views.timeline, name='timeline'),
+    url(r'^edit_profile$', views.edit_profile, name='edit'),
 ]
 
 
--- instaclone/views.py
+++ instaclone/views.py
@@ -51,18 +51,65 @@ def timeline(request):
         return redirect('timeline')
     elif request.method=='POST' and 'comment' in request.POST:
         comment=Comment(comment=request.POST.get("comment"),
-                        image=int(request.POST.get("imageed")),
+                        image=int(request.POST.get("imaged")),
                         username=request.POST.get("user"),
                         count=0)
         comment.save()
         comment.count=F('count')+1
         return redirect('timeline')
     elif request.method=='POST' and 'image' in request.POST:
-        posted=request.POST.get("image")
+        posted=request.POST.get("post")
         for image in images:
-            if (int(image.id)==int(posted)):
+            if (int(image.id)==int(imaged)):
                 image.like+=1
                 image.save()
         return redirect('timeline')
     else:
         return render(request, 'timeline.html',{"users":users,"follows":follows,"images":images,"comments":comments})
+
+
+@login_required(login_url='/accounts/login/')
+def edit_profile(request):
+    current_user = request.user
+    if request.method == 'POST':
+        form = DetailsForm(request.POST, request.FILES)
+        if form.is_valid():
+                Profile.objects.filter(id=current_user.profile.id).update(bio=form.cleaned_data["bio"])
+                profile = Profile.objects.filter(id=current_user.profile.id).first()
+                profile.profile_pic.delete()
+                profile.profile_pic=form.cleaned_data["profile_pic"]
+                profile.save()
+        return redirect('profile')
+
+    else:
+        form = FormDetails()
+    
+    return render(request, 'instapp/_profile.html',{"form": form})
+
+@login_required(login_url='/accounts/login/')
+def other_profile(request,id):
+    profile_user=User.objects.filter(id=id).first()
+    posts=Image.objects.all()
+    following=Following.objects.filter(username=profile_user.username).all()
+    followingcount=len(following)
+    followers=Following.objects.filter(followed=profile_user.username).all()
+    followercount=len(followers)
+    return render(request, 'instapp/other_profile.html',{"profile_user": profile_user,"posts":posts,"followingcount":followingcount,"followercount":followercount})
+
+@login_required(login_url='/accounts/login/')
+def search(request):
+    posts=Image.objects.all()
+    if 'username' in request.GET and request.GET["username"]:
+        search_term = request.GET.get("username")
+        following=Following.objects.filter(username=search_term).all()
+        followingcount=len(following)
+        followers=Following.objects.filter(followed=search_term).all()
+        followercount=len(followers)
+        searched_user = User.objects.filter(username=search_term).first()
+        if searched_user:
+            message = f"{search_term}"
+            return render(request, 'instapp/other_profile.html',{"profile_user": searched_user,"posts":posts,"followingcount":followingcount,"followercount":followercount})
+        else:
+            message = "The username you are searching for does not exist.Thank you for visiting InstaNight."
+            return render(request, 'instapp/fourfor.html',{"message":message})
+    
